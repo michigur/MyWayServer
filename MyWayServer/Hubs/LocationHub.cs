12345c4728b14644cad1660392274d41c464e030
User <User@LAB3-4>
2022-06-07 12:03:20 +0300

    SugnalR

--- MyWayServer/Hubs/LocationHub.cs
+++ MyWayServer/Hubs/LocationHub.cs
@@ -3,28 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
-
+using MyWayServer.Models;
 
 namespace MyWayServer.Hubs
 {
     public class LocationHub: Hub
     {
-
+        private MyWayContext context;
+        public LocationHub(MyWayContext context)
+        {
+            this.context = context;
+        }
+        //This message is sent by the customer to the car, so the car can start driving
         public async Task SendOnBoard(int carId, int ClientId)
         {
             IClientProxy proxy = Clients.Group(carId.ToString());
-            await proxy.SendAsync("UpdateKidOnBoard", ClientId);
+            await proxy.SendAsync("UpdateOnBoard", ClientId);
         }
+        //This message is sent by the car to whom ever neds it including the customer app
         public async Task SendLocation(int carId, double longitude, double latitude)
         {
             IClientProxy proxy = Clients.Group(carId.ToString());
             await proxy.SendAsync("UpdateDriverLocation", longitude, latitude);
         }
+        //this message is sent by the customer to the car before paying and going out of the car
         public async Task SendArriveToDestination(int carId)
         {
             IClientProxy proxy = Clients.Group(carId.ToString());
             await proxy.SendAsync("UpdateArriveToDestination");
+            //Update the availability of the car in the DB
+            //Car c = this.context.Cars.Where(c => c.CarId == carId).FirstOrDefault();
+            //if (c != null)
+            //{
+            //    c.IsAvailable = true;
+            //    this.context.Entry(c).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            //    this.context.SaveChanges();
+            //}
+            
         }
+        //used by all apps
         public async Task OnConnect(int carId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, carId.ToString());
@@ -37,62 +54,13 @@ namespace MyWayServer.Hubs
         //    await base.OnConnectedAsync();
         //}
 
+        //sent by the customer after payment and going out of the car
         public async Task OnDisconnect(int carId)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, carId.ToString());
             await base.OnDisconnectedAsync(null);
         }
 
-        //public async Task OnDisconnect(string groupName)
-        //{
-        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-        //    await base.OnDisconnectedAsync(null);
-        //}
-
-        public async Task StartDrive(int carId)
-        {
-            IClientProxy proxy = Clients.Group(carId.ToString());
-            await proxy.SendAsync("StartDrive");
-        }
-
-
-
-
-
-
-
-
-
-        //public async Task UpdateLocation(int? routeid, string latitude, string longitude)
-        //{
-
-        //        IClientProxy proxy = Clients.Group(routeid.ToString());
-        //        await proxy.SendAsync("UpdateCarLocation", latitude, longitude);
-
-        //}
-
-        //public async Task SendMessageToGroup(string user, string message, string groupName)
-        //{
-        //    IClientProxy proxy = Clients.Group(groupName);
-        //    await proxy.SendAsync("ReceiveMessageFromGroup", user, message, groupName);
-        //}
-        //public async Task SendMessage(string user, string message)
-        //{
-        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
-        //}
-
-        //public async Task OnConnect(string[] groupNames)
-        //{
-        //    foreach (string groupName in groupNames)
-        //        await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-        //    await base.OnConnectedAsync();
-        //}
-
-        //public async Task OnDisconnect(string[] groupNames)
-        //{
-        //    foreach (string groupName in groupNames)
-        //        await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
-        //    await base.OnDisconnectedAsync(null);
-        //}
+        
     }
 }
